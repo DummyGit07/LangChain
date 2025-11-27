c50a781a6505df7253a8c692f1d4a16c1b6db833
DummyGit07 <dummymailwork07@gmail.com>
2025-11-27 10:51:12 +0530

    Update travel_guide.py

--- travel_guide.py
+++ travel_guide.py
@@ -5,20 +5,29 @@ import streamlit as st
 llm = ChatOllama(model = "gemma:2b")
 
 prompt_template = PromptTemplate(
-    input_variables=['position', 'company', 'strengths', 'weaknesses'],
-    template= """You are a career coach. Provide tailored interview tips for the
-                position of {position} at {company}.
-                Highlight your strengths in {strengths} and prepare for questions
-                about your weaknesses such as {weaknesses}.
-            """
+    input_variables=["city","month","language","budget"],
+    template="""Welcome to the {city} travel guide!
+    If you're visiting in {month}, here's what you can do:
+    1. Must-visit attractions.
+    2. Local cuisine you must try.
+    3. Useful phrases in {language}.
+    4. Tips for traveling on a {budget} budget.
+    Enjoy your trip!
+    """
 )
 
-st.title("InterView Tips Generator")
-position = st.text_input("Enter Job Position:")
-company = st.text_input("Enter Company:")
-strengths = st.text_area("Enter your Strengths:", height=100)
-weaknesses = st.text_area("Enter Your Weaknesses: ", height=100)
+st.title("Travel Guide")
 
-if position and company and strengths and weaknesses:
-    response = llm.invoke(prompt_template.format(position=position, company=company, strengths=strengths, weaknesses=weaknesses))
+city = st.text_input("Enter the city:")
+month = st.text_input("Enter the month of travel")
+language = st.text_input("Enter the language:")
+budget = st.selectbox("Travel Budget",["Low","Medium","High"])
+
+
+if city and month and language and budget:
+    response = llm.invoke(prompt_template.format(city=city,
+                                                 month=month,
+                                                 language=language,
+                                                 budget=budget
+                                                 ))
     st.write(response.content)
