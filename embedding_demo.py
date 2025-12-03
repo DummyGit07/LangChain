c74b9d922dcac79ceca88dee27786003797ddd44
DummyGit07 <dummymailwork07@gmail.com>
2025-12-03 23:20:35 +0530

    Delete embedding_demo.py

--- embedding_demo.py
+++
@@ -1,9 +0,0 @@
-# additional library require,       pip install angchain-ollama
-
-from langchain_ollama import OllamaEmbeddings
-
-llm = OllamaEmbeddings(model="llama3.2")
-
-question = input("Enter: ")
-response = llm.embed_query(question)
-print(response)
