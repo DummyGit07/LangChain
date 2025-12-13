4a8ab1c43b557412b5d405c9bcecb0348c93321d
DummyGit07 <dummymailwork07@gmail.com>
2025-12-13 11:17:52 +0530

    Add files via upload

---
+++ images_analysis/images_demo.py
@@ -0,0 +1,43 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+
+import base64
+import os
+
+
+def encode_image(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode()
+
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+llm = ChatOpenAI(model="gpt-4o", api_key=OPENAI_API_KEY)
+# image = encode_image("")
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a helpful assistant that can describe images."),
+        (
+            "human",
+            [
+                {"type": "text", "text": "{input}"},
+                {
+                    "type": "image_url",
+                    "image_url": {
+                        "url": f"data:image/jpeg;base64,""{image}",
+                        "detail": "low",
+                    },
+                },
+            ],
+        ),
+    ]
+)
+
+chain = prompt | llm
+
+uploaded_image = st.file_uploader("Import Image", type=['jpg','png'])
+question = st.text_input("Ask question related to Image: ")
+
+if question:
+    image = encode_image(uploaded_image)
+    response = chain.invoke({"input":{question}, "image":image})
+    st.write(response.content)
